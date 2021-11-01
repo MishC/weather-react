1f9926df7f84a14d835eaf3a28827a58392829ef
MishC <michaela.chovancova@gmail.com>
2021-11-01 22:22:39 +0100

    styles (all centered)

--- src/Weather.js
+++ src/Weather.js
@@ -39,7 +39,7 @@ export default function Weather(props) {
       </ul>
 
       <div className="row row1">
-        <div className="col-2 offset-lg-2 offset-md-1 col-sm-auto row1">
+        <div className="col-3 offset-lg-1 offset-md-1 col-sm-auto row1">
           <ul className="temperature">
             <li className="temp">{props.temperature}</li>
             <li>
@@ -55,7 +55,7 @@ export default function Weather(props) {
             </li>
           </ul>
         </div>
-        <div className="col-2 col-md-2 col-sm-auto row1">
+        <div className="col-4 col-md-4 col-sm-auto row1">
           <h1 className="card-title">
             <img src={props.icon} alt={props.description} className="icon" />
           </h1>
@@ -63,7 +63,7 @@ export default function Weather(props) {
             <h6 className="iconic text-capitalize">{props.description}</h6>
           </footer>
         </div>
-        <div className="col-2 col-md-2 col-sm-auto row1">
+        <div className="col-4 col-md-4 col-sm-auto row1">
           <ul className="card-text">
             <li>
               <h6>Precipitation: {props.precipitation} %</h6>
