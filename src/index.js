53bb102cdb54854e9e605652f17fac88aed0128a
MishC <michaela.chovancova@gmail.com>
2021-11-01 14:21:19 +0100

    css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
