245f1ab6bc8111eb2d053f83d9c3b567af4ebef5
MishC <michaela.chovancova@gmail.com>
2021-11-02 21:07:21 +0100

    css offset gone

--- src/Weather.js
+++ src/Weather.js
@@ -5,28 +5,6 @@ import "./styles/Weather.css";
 import "./styles/Mediascreen.css";
 
 export default function Weather(props) {
-  /* let [styleC, setStyleC] = useState({ color: "#1ab2a8" });
-  let [styleF, setStyleF] = useState({ color: "#1ab2a8" });
-  //let [temp, setTemp] = useState(props.temperature);
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    //let temperatureF = Math.round((props.temperature * 9) / 5 + 32);
-
-    //setTemp(temperatureF);
-
-    setStyleF({ color: "#D67256" });
-    setStyleC({ color: "#1ab2a8" });
-  }
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-
-    // setTemp(props.temperature);
-    setStyleC({ color: "#D67256" });
-    setStyleF({ color: "#1ab2a8" });
-  }*/
-
   return (
     <div className="result Weather">
       <ul className="strip strip1">
@@ -39,10 +17,10 @@ export default function Weather(props) {
       </ul>
 
       <div className="row row1">
-        <div className="col-3 offset-lg-1 offset-md-1 col-sm-auto row1">
+        <div className="col col-sm-auto row1">
           <WeatherTemperature temperature={props.temperature} />
         </div>
-        <div className="col-4 col-md-4 col-sm-auto row1">
+        <div className="col col-sm-auto row1">
           <h1 className="card-title">
             <img src={props.icon} alt={props.description} className="icon" />
           </h1>
@@ -50,7 +28,7 @@ export default function Weather(props) {
             <h6 className="iconic text-capitalize">{props.description}</h6>
           </footer>
         </div>
-        <div className="col-4 col-md-4 col-sm-auto row1">
+        <div className="col col-sm-auto row1">
           <ul className="card-text">
             <li>
               <h6>Precipitation: {props.precipitation} %</h6>
